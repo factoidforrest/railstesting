0f86801854b85ee07a24945dd8415a48f0e8dd23
Forrest Allison <light24bulbs@gmail.com>
2012-06-03 16:14:23 -0700

    where are my files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railstesting::Application.config.secret_token = 'aae9be4e093abcea6d9db6dd73c06bfa00cba697178f146b8bcd41de96b184796e3a178be6ffb3ac3b3605b38472bab22d3f4a70e9523f5b46df2218c9855993'
